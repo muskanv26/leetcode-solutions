47c897fe24943bd2402729cc3f420181286c3ae4
muskanv26 <varshneymuskan2602@gmail.com>
2026-03-05 21:32:14 +0530

    Update LeetCode-1758-Minimum-Changes-To-Make-Alternating-Binary-String.java

--- Strings/LeetCode-1758-Minimum-Changes-To-Make-Alternating-Binary-String.java
+++ Strings/LeetCode-1758-Minimum-Changes-To-Make-Alternating-Binary-String.java
@@ -1,3 +1,24 @@
+/*
+LeetCode 1758
+Minimum Changes to Make Alternating Binary String
+
+Problem:
+Given a binary string s, return the minimum number of character changes required
+to make the string alternating.
+
+An alternating string is one where no two adjacent characters are the same.
+
+Approach:
+We check two possible patterns:
+1. Start with '0' -> 010101...
+2. Start with '1' -> 101010...
+
+We count mismatches for both patterns and return the minimum.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
 class Solution 
 {
     public int minOperations(String s) 
