940b1b901e92ccabddee5801f58566eeccd16bf2
muskanv26 <varshneymuskan2602@gmail.com>
2026-03-06 23:06:58 +0530

    Update 1784_Check_if_Binary_String_Has_at_Most_One_Segment_of_Ones.java

--- Strings/1784_Check_if_Binary_String_Has_at_Most_One_Segment_of_Ones.java
+++ Strings/1784_Check_if_Binary_String_Has_at_Most_One_Segment_of_Ones.java
@@ -1,3 +1,16 @@
+/*
+Problem: 1784. Check if Binary String Has at Most One Segment of Ones
+Platform: LeetCode
+Link: https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/
+
+Approach:
+Traverse the string and check if '1' appears after a '0'.
+If that happens, it means a new segment of ones started.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
 class Solution {
     public boolean checkOnesSegment(String s) {
         for(int i = 1; i < s.length(); i++) 
